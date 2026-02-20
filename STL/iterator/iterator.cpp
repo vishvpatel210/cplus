2e4214090efe4090ad4d35d99d8edf465c49edfe
vishvpatel210 <vishv.patel.cg@gmail.com>
2026-02-20 10:15:27 +0530

    new

--- STL/iterator/iterator.cpp
+++ STL/iterator/iterator.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<list>
 
 using namespace std;
 
@@ -12,4 +13,16 @@ int main(){
         cout << "forward iterator current value: " << *itr << endl;
         ++itr;
     }
+    cout << "list element are begin printed one by one..." << endl;
+
+    list <int> l1 = {1,2,3,4,5,6,-90,67};
+
+   auto itr1 = l1.begin();
+
+      while(itr1 != l1.end()){
+        cout << "forward iterator current value: " << *itr1 << endl;
+        ++itr1;
+    }
+
+    return 0;
 }
